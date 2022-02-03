9460b77cef63312df3a7fec16fc65f4b7f7e022a
ranjeethmd <ranjeeth.devaiah@outlook.com>
2022-02-03 02:00:39 -0800

    Elsa init

---
+++ pacakage.bat
@@ -0,0 +1,6 @@
+dotnet add package Elsa
+dotnet add package Elsa.Activities.Http
+dotnet add package Elsa.Activities.Temporal.Quartz
+dotnet add package Elsa.Persistence.EntityFramework.Sqlite
+dotnet add package Elsa.Server.Api
+dotnet add package Elsa.Designer.Components.Web
